20b3484e4770288ad35e7edf9215f81a143f494f
zhlgdl <59690749+zhlgdl@users.noreply.github.com>
2020-04-17 09:52:28 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,4 +7,4 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/10.10.10.1age/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.1/g' pack/base-files/files/bin/config_generate
